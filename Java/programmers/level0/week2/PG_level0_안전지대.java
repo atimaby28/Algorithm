0f2aa2846a8e8eed1745aa3700e35451ff4ac66a
atimaby28 <atimaby28@gmail.com>
2023-11-23 22:53:42 +0900

    Update : Algorithm

--- Java/programmers/level0/week2/PG_level0_안전지대.java
+++ Java/programmers/level0/week2/PG_level0_안전지대.java
@@ -9,27 +9,27 @@ import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class PG_level0_안전지대 {
-	
-	public static int[] dx = {0, 0, -1, 1, -1, 1, 1, -1};
-	public static int[] dy = {1, -1, 0, 0, -1, -1, 1, 1};
-	
+
+	public static int[] dx = { 0, 0, -1, 1, -1, 1, 1, -1 };
+	public static int[] dy = { 1, -1, 0, 0, -1, -1, 1, 1 };
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		StringTokenizer st = null;
-		
+
 		int n = Integer.parseInt(br.readLine());
-		
+
 		int[][] board = new int[n][n];
-		
-		st = new StringTokenizer(br.readLine());
+
 		for (int i = 0; i < board.length; i++) {
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < board[i].length; j++) {
 				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+
 		int result = solution(board);
 
 		bw.write(result + "\n");
@@ -38,26 +38,49 @@ public class PG_level0_안전지대 {
 		bw.close();
 
 	}
-	
-    public static int solution(int[][] board) {
-        int answer = 0;
-        
-        for (int i = 0; i < board.length; i++) {
+
+	public static int solution(int[][] board) {
+		int answer = board.length * board.length;
+
+		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
-				if(board[i][j] == 1) {
-					
+				if (board[i][j] == 1) {
+
 					int nx = 0;
 					int ny = 0;
-					
+
 					for (int g = 0; g < 8; g++) {
 						nx = j + dx[g];
 						ny = i + dy[g];
+
+//						if (nx < 0 || nx > board[i].length - 1 || ny < 0 || ny > board[i].length - 1) {
+//							continue;
+//						} else {
+//							if (board[ny][nx] == 1) {
+//								continue;
+//							}
+//
+//							board[ny][nx] = 2;
+//						}
+						
+						if(nx >=0 && nx < board.length && ny >=0 && ny <board[i].length && board[ny][nx] == 0) {
+							board[ny][nx] = 2;
+						}
+						
 					}
 				}
 			}
 		}
-        
-        return answer;
-    }
-    
+
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				if (board[i][j] == 2 || board[i][j] == 1) {
+					answer--;
+				}
+			}
+		}
+
+		return answer;
+	}
+
 }
