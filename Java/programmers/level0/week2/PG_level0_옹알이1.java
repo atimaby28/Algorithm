6e528a31d4ffbbbba079266e86bfca50462db0a0
atimaby28 <atimaby28@gmail.com>
2023-11-25 21:30:52 +0900

    Update : Algorithm

--- Java/programmers/level0/week2/PG_level0_옹알이1.java
+++ Java/programmers/level0/week2/PG_level0_옹알이1.java
@@ -1,10 +1,59 @@
 package level0.week2;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class PG_level0_옹알이1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = null;
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		String[] babbling = new String[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < babbling.length; i++) {
+			babbling[i] = st.nextToken();
+		}
+		
+		int result = solution(babbling);
+		
+		bw.write(result + " ");
+		
+		bw.flush();
+		bw.close();
+		
 	}
+	
+    public static int solution(String[] babbling) {
+        int answer = 0;
+        
+        for (int i = 0; i < babbling.length; i++) {
+        	babbling[i] = babbling[i].replaceAll("aya", "*");
+        	babbling[i] = babbling[i].replaceAll("ye", "*");
+        	babbling[i] = babbling[i].replaceAll("woo", "*");
+        	babbling[i] = babbling[i].replaceAll("ma", "*");
+		}
+        
+        for (int i = 0; i < babbling.length; i++) {
+        	babbling[i] = babbling[i].replaceAll("[\\*]", "");
+		}
+        
+        for (int i = 0; i < babbling.length; i++) {
+			if(babbling[i].isEmpty()) {
+				answer++;
+			}
+		}
+        
+        return answer;
+    }
 
 }
