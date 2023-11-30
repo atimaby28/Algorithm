b40067e3297da256adaeaa0d10d8f0d3640666d2
atimaby28 <atimaby28@gmail.com>
2023-11-30 23:45:55 +0900

    Update : Algorithm

---
+++ Java/programmers/level0/week1/PG_level0_두수의합.java
@@ -0,0 +1,37 @@
+package level0.week1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class PG_level0_두수의합 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int num1 = Integer.parseInt(st.nextToken());
+		int num2 = Integer.parseInt(st.nextToken());
+		
+		int result = solution(num1, num2);
+		
+		bw.write(result + "\n");
+		
+		bw.flush();
+		bw.close();
+	}
+	
+    public static int solution(int num1, int num2) {
+        int answer = -1;
+        
+        answer = num1 + num2;
+        
+        return answer;
+    }
+
+}
--- Java/programmers/level0/week2/PG_level0_잘라서배열로저장하기.java
+++ Java/programmers/level0/week2/PG_level0_잘라서배열로저장하기.java
@@ -13,27 +13,42 @@ public class PG_level0_잘라서배열로저장하기 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		String my_str = br.readLine();
-		
+
 		int n = Integer.parseInt(br.readLine());
-		
+
 		String[] result = solution(my_str, n);
-		
+
 		bw.write(result + "\n");
-		
+
 		bw.flush();
 		bw.close();
-		
+
 	}
-	
-    public static String[] solution(String my_str, int n) {
-        String[] answer = new String[my_str.length() / n];
-        
-        int i = 0;
-		for (int j = 0; j < my_str.length(); j++) {
-			answer[i++] += my_str.charAt(j);
+
+	public static String[] solution(String my_str, int n) {
+		String[] answer = {};
+
+		int len = 0;
+		if (my_str.length() % 2 == 0) {
+			len = my_str.length() / n + 1;
+		} else {
+			len = my_str.length() / n;
 		}
-        
-        return answer;
-    }
+
+		answer = new String[len];
+
+		int k = 0;
+		for (int i = 0; i < len; i++) {
+			answer[i] = ""; 
+			for (int j = 0; j < n; j++) {
+				if(k > my_str.length() - 1) {
+					break;
+				}
+				answer[i] += my_str.charAt(k++);
+			}
+		}	
+
+		return answer;
+	}
 
 }
