1ee79fdfa383f786025b8533af53aa41311feb53
atimaby28 <atimaby28@gmail.com>
2024-03-14 23:51:47 +0900

    Update : Algorithm

---
+++ Java/programmers/level2/week2/PG_level2_전화번호목록.java
@@ -0,0 +1,70 @@
+package level2.week2;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class PG_level2_전화번호목록 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = null;
+
+        int k = Integer.parseInt(br.readLine());
+        String[] phone_book = new String[k];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < phone_book.length; i++) {
+            phone_book[i] = st.nextToken();
+        }
+
+        boolean result = solution(phone_book);
+
+        bw.write(result + "\n");
+
+        bw.flush();
+        bw.close();
+
+    }
+
+    public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        HashMap<String, Boolean> hsmap = new HashMap<>();
+
+        Arrays.sort(phone_book, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() > o2.length()) {
+                    return 1;
+                } else if(o1.length() < o2.length()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+
+        for (int i = 0; i < phone_book.length; i++) {
+            int sampleLen = phone_book[i].length();
+
+            for (int j = i; j < phone_book.length; j++) {
+                String key = phone_book[j].substring(0, sampleLen);
+                if(hsmap.containsKey(key)) {
+                    answer = false;
+                    break;
+                } else {
+                    hsmap.put(key, true);
+                }
+            }
+            hsmap.clear();
+        }
+
+        return answer;
+    }
+
+}
