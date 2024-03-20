a8a2338227a22720f2f435cd52d0e50e6e32bf94
atimaby28 <atimaby28@gmail.com>
2024-03-20 10:06:55 +0900

    Update : Algorithm

--- Java/programmers/level2/week2/PG_level2_오픈채팅방.java
+++ Java/programmers/level2/week2/PG_level2_오픈채팅방.java
@@ -1,2 +1,63 @@
-package level2.week2;public class PG_level2_오픈채팅방 {
+package level2.week2;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class PG_level2_오픈채팅방 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int k = Integer.parseInt(br.readLine());
+        String[] record = new String[k];
+
+        for (int i = 0; i < record.length; i++) {
+            record[i] = br.readLine();
+        }
+
+        String[] result = solution(record);
+
+        bw.write(result + "\n");
+
+        bw.flush();
+        bw.close();
+
+    }
+
+    public static String[] solution(String[] record) {
+        String[] answer = {};
+
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        String[][] log = new String[record.length][3];
+
+        String enter = "님이 들어왔습니다.";
+        String exit = "님이 나갔습니다.";
+
+        int changeCnt = 0;
+        for (int i = 0; i < record.length; i++) {
+            log[i] = record[i].split(" ");
+
+            if(!log[i][0].equals("Leave")) {
+                hashMap.put(log[i][1], log[i][2]);
+            }
+            if(log[i][0].equals("Change")) {
+                changeCnt++;
+            }
+        }
+
+        answer = new String[record.length - changeCnt];
+
+        int k = 0;
+        for (int i = 0; i < record.length; i++) {
+            if(log[i][0].equals("Enter")) {
+                answer[k++] = hashMap.get(log[i][1]) + enter;
+            } else if (log[i][0].equals("Leave")) {
+                answer[k++] = hashMap.get(log[i][1]) + exit;
+            }
+        }
+        return answer;
+    }
+
 }
--- Java/programmers/level2/week2/PG_level2_주식가격.java
+++ Java/programmers/level2/week2/PG_level2_주식가격.java
@@ -1,2 +1,64 @@
-package level2.week2;public class PG_level2_주식가격 {
+package level2.week2;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class PG_level2_주식가격 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = null;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] numbers = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] result = solution(numbers);
+
+        bw.write(result + "\n");
+
+        bw.flush();
+        bw.close();
+
+    }
+
+    public static int[] solution(int[] prices) {
+        int[] answer = {};
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < prices.length; i++) {
+            int count = 0;
+            for (int j = i + 1; j < prices.length; j++) {
+                if(prices[i] <= prices[j]) {
+                    count++;
+                } else {
+                    count += 1;
+                    break;
+                }
+            }
+
+            queue.add(count);
+        }
+
+        answer = new int[queue.size()];
+
+        int i = 0;
+        while (!queue.isEmpty()) {
+            answer[i++] = queue.poll();
+        }
+
+        return answer;
+    }
+
 }
