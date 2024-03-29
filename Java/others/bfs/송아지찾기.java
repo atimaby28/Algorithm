512b959286c8d777def7856044345de499aebc19
atimaby28 <atimaby28@gmail.com>
2024-03-29 23:04:55 +0900

    Update : Algorithm

--- Java/others/bfs/송아지찾기.java
+++ Java/others/bfs/송아지찾기.java
@@ -1,2 +1,75 @@
-package bfs;public class 송아지찾기 {
+package bfs;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class 송아지찾기 {
+    public static int ans = 0;
+    public static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int s = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        int result = solution(s, e);
+
+        bw.write(result + "\n");
+
+        bw.flush();
+        bw.close();
+
+    }
+
+    private static int solution(int s, int e) {
+        int answer = 0;
+
+        visited = new boolean[10001];
+
+        bfs(s, e);
+
+        answer = ans;
+
+        return answer;
+    }
+
+    private static void bfs(int s, int e) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+
+        boolean flag = false;
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+
+            ans++;
+
+            for (int i = 0; i < len; i++) {
+                int idx = queue.poll();
+
+                if(idx + 1 == e || idx - 1 == e || idx + 5 == e) {
+                    flag = true;
+                    break;
+                }
+
+                if(idx >= 1 && idx < 10000 && !visited[idx]) {
+                    queue.add(idx + 1);
+                    queue.add(idx - 1);
+                    queue.add(idx + 5);
+                }
+
+                visited[idx] = true;
+            }
+
+            if(flag) {
+                break;
+            }
+
+        }
+    }
 }
--- Java/programmers/level2/week3/PG_level2_큰수만들기.java
+++ Java/programmers/level2/week3/PG_level2_큰수만들기.java
@@ -1,2 +1,40 @@
-package level2.week3;public class 큰수만들기 {
+package level2.week3;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PG_level2_큰수만들기 {
+
+    public static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String number = br.readLine();
+        int k = Integer.parseInt(br.readLine());
+
+        String result = solution(number, k);
+
+        bw.write(result + "\n");
+
+        bw.flush();
+        bw.close();
+
+    }
+
+
+    public static String solution(String number, int k) {
+        String answer = "";
+
+        int startIdx = number.length() - k;
+
+        for (int i = 0; i < startIdx; i++) {
+            System.out.println(startIdx);
+        }
+
+        return answer;
+    }
+
 }
