ffb29947cf4ef0b5d5242b0218bbf265d216877f
atimaby28 <atimaby28@gmail.com>
2024-04-13 23:18:34 +0900

    Update : Algorithm

--- Java/others/dfs/미로탐색.java
+++ Java/others/dfs/미로탐색.java
@@ -1,2 +1,66 @@
-package dfs;public class 미로탐색 {
+package dfs;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 미로탐색 {
+
+    public static int ans;
+    public static int[][] map;
+    public static boolean[][] visted;
+
+    public static int[] dx = {0, 0, -1, 1};
+    public static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = null;
+
+        int n = Integer.parseInt(br.readLine());
+
+        map = new int[n + 1][n + 1];
+        visted = new boolean[n + 1][n + 1];
+
+        for (int i = 1; i < map.length; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < map[i].length; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        visted[1][1] = true;
+        dfs(1, 1);
+
+        int result = ans;
+
+        bw.write(result + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void dfs(int y, int x) {
+        if(y == 7 && x == 7) {
+            ans++;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int cx = x + dx[i];
+            int cy = y + dy[i];
+
+            if(cx >= 1 && cx < map.length && cy >= 1 && cy < map.length) {
+                if(map[cy][cx] == 0 && !visted[cy][cx]) {
+                    visted[cy][cx] = true;
+                    dfs(cy, cx);
+                    visted[cy][cx] = false;
+                }
+            }
+        }
+
+    }
+
+
 }
--- Java/programmers/level0/week6/PG_level0_구슬을나누는경우의수.java
+++ Java/programmers/level0/week6/PG_level0_구슬을나누는경우의수.java
@@ -1,2 +1,42 @@
-package level0.week6;public class PG_level0_구슬을나누는경우의수 {
+package level0.week6;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class PG_level0_구슬을나누는경우의수 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int balls = Integer.parseInt(st.nextToken());
+        int share = Integer.parseInt(st.nextToken());
+
+        int result = solution(balls, share);
+
+        bw.write(result + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+
+
+    public static int solution(int balls, int share) {
+        int answer = 0;
+
+        answer = combination(balls, share);
+
+        return answer;
+    }
+
+    private static int combination(int n, int r) {
+        if(r == 0 || n == r) {
+            return  1;
+        }
+
+        return combination(n-1, r-1) + combination(n-1, r);
+    }
+
 }
