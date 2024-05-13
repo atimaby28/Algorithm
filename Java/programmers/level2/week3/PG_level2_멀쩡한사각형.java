000d79264d40fa5c7ccfb3fdaa4622c4e29b2c60
atimaby28 <atimaby28@gmail.com>
2024-05-13 20:04:47 +0900

    Update : Algorithm

--- Java/programmers/level2/week3/PG_level2_멀쩡한사각형.java
+++ Java/programmers/level2/week3/PG_level2_멀쩡한사각형.java
@@ -25,9 +25,33 @@ public class PG_level2_멀쩡한사각형 {
     public static long solution(int w, int h) {
         long answer = 1;
 
-        
+        int nw = Math.min(w, h);
+        int nh = Math.max(w, h);
+
+//      최대 공약수 -> (패턴 반복수)
+        int gcd = gcd(nw, nh);
+
+        int minW = nw / gcd;
+        int minH = nh / gcd;
+
+        /*
+        minW * minH 에서 대각선을 지나지 않는 크기는 (minW-1) * (minH-1)
+        cut: 패턴의 한부분 minW * minH 에서 잘리지 않는 부분을 뺀 것
+        실제로는 w + h -1 과 같다.
+        */
+
+        int cut = (minW * minH) - ((minW - 1) * (minH - 1));
+
+        answer = ((long) w * h) - ((long) cut * gcd);
 
         return answer;
+    }
+
+    private static int gcd(int nw, int nh) {
+        if(nh == 0) {
+            return nw;
+        }
+        return gcd(nh, nw % nh);
     }
 
 }
