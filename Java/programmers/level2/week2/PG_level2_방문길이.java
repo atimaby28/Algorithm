a8896192c2d4c7010675a7372508ec7edf2cfdff
atimaby28 <atimaby28@gmail.com>
2024-06-06 01:58:09 +0900

    Update : Algorithm

--- Java/programmers/level2/week2/PG_level2_방문길이.java
+++ Java/programmers/level2/week2/PG_level2_방문길이.java
@@ -4,6 +4,10 @@ import java.io.*;
 import java.util.HashMap;
 
 public class PG_level2_방문길이 {
+
+    public static int[] dx = {0, 0, -1, 1};
+    public static int[] dy = {-1, 1, 0, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -19,53 +23,46 @@ public class PG_level2_방문길이 {
     }
 
     public static int solution(String dirs) {
+
         int answer = 0;
+        boolean[][][] map = new boolean[11][11][4];
 
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-
-        hashMap.put(1, 2);
-        hashMap.put(2, 1);
-        hashMap.put(3, 4);
-        hashMap.put(4, 3);
-
-        boolean[][][] visited = new boolean[11][11][5];
-        int x = 5, y = 5;
+        int x = 5;
+        int y = 5;
 
         for (int i = 0; i < dirs.length(); i++) {
-            char c = dirs.charAt(i);
+            char comm = dirs.charAt(i);
+            int dir = 0;
+            int reverseDir = 0;
 
-            int nx = x, ny = y;
-            int idxC = 0;
-
-            if(c == 'U') {
-                ny = y + 1;
-                idxC = 1;
-            } else if(c == 'D') {
-                ny = y - 1;
-                idxC = 2;
-            } else if(c == 'L') {
-                nx = x - 1;
-                idxC = 3;
-            } else if(c == 'R') {
-                nx = x + 1;
-                idxC = 4;
+            if (comm == 'U') {
+                dir = 0;
+                reverseDir = 1;
+            } else if (comm == 'D') {
+                dir = 1;
+                reverseDir = 0;
+            } else if (comm == 'L') {
+                dir = 2;
+                reverseDir = 3;
+            } else if (comm == 'R') {
+                dir = 3;
+                reverseDir = 2;
             }
 
-            if(ny >=0 && ny < visited.length && nx >= 0 && nx < visited.length) {
+            if (x + dx[dir] < 0 || x + dx[dir] >= map[0].length || y + dy[dir] < 0 || y + dy[dir] >= map.length)
+                continue;
 
-                if(!visited[nx][ny][idxC]) {
-                    visited[nx][ny][idxC] = true;
-                    visited[x][y][hashMap.get(idxC)] = true;
-                    answer++;
-                }
-
-                x = nx;
-                y = ny;
+            if (!map[y][x][dir] && !map[y + dy[dir]][x + dx[dir]][reverseDir]) {
+                map[y][x][dir] = true;
+                map[y + dy[dir]][x + dx[dir]][reverseDir] = true;
+                answer++;
             }
+
+            x += dx[dir];
+            y += dy[dir];
 
         }
 
         return answer;
     }
-
 }
