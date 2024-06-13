535520fc9a53d344cfbfe333a1eba95c255809b7
atimaby28 <atimaby28@gmail.com>
2024-06-13 10:17:05 +0900

    Update : Algorithm

--- Java/programmers/level2/week2/PG_level2_롤케이크자르기.java
+++ Java/programmers/level2/week2/PG_level2_롤케이크자르기.java
@@ -29,71 +29,95 @@ public class PG_level2_롤케이크자르기 {
         bw.close();
     }
 
+//    public static int solution(int[] topping) {
+//        int answer = -1;
+//
+//        HashMap<Integer, Integer> chulSu = new HashMap<>();
+//        HashMap<Integer, Integer> dongSaeng = new HashMap<>();
+//
+//        int mid = topping.length / 2;
+//
+//        int count = 0;
+//        for (int i = 0; i < mid; i++) {
+//            chulSu.put(topping[i], chulSu.getOrDefault(topping[i], 0) + 1);
+//        }
+//
+//        for (int i = mid; i < topping.length; i++) {
+//            dongSaeng.put(topping[i], dongSaeng.getOrDefault(topping[i], 0) + 1);
+//        }
+//
+//        if(chulSu.size() == dongSaeng.size()) {
+//            count++;
+//        }
+//
+//        HashMap<Integer, Integer> chulSuCopy1 = new HashMap<>(chulSu);
+//        HashMap<Integer, Integer> dongSaengCopy1 = new HashMap<>(dongSaeng);
+//
+//        for (int i = 0; i < mid; i++) {
+//            chulSuCopy1.put(topping[mid - i], chulSuCopy1.getOrDefault(topping[mid - i], 0) - 1);
+//            dongSaengCopy1.put(topping[mid - i], dongSaengCopy1.getOrDefault(topping[mid - i], 0) + 1);
+//
+//            if(chulSuCopy1.get(topping[mid - i]) == 0 ) {
+//                chulSuCopy1.remove(topping[mid - i]);
+//            }
+//
+//            if(dongSaengCopy1.get(topping[mid - i]) == 0 ) {
+//                dongSaengCopy1.remove(topping[mid - i]);
+//            }
+//
+//            if(chulSuCopy1.size() == dongSaengCopy1.size()) {
+//                count++;
+//            }
+//
+//        }
+//
+//        HashMap<Integer, Integer> chulSuCopy2 = new HashMap<>(chulSu);
+//        HashMap<Integer, Integer> dongSaengCopy2 = new HashMap<>(dongSaeng);
+//
+//        for (int i = mid; i < topping.length - 1; i++) {
+//            chulSuCopy2.put(topping[i], chulSuCopy2.getOrDefault(topping[i], 0) + 1);
+//            dongSaengCopy2.put(topping[i], dongSaengCopy2.getOrDefault(topping[i], 0) - 1);
+//
+//            if(chulSuCopy2.get(topping[i]) == 0 ) {
+//                chulSuCopy2.remove(topping[i]);
+//            }
+//
+//            if(dongSaengCopy2.get(topping[i]) == 0 ) {
+//                dongSaengCopy2.remove(topping[i]);
+//            }
+//
+//            if(chulSuCopy2.size() == dongSaengCopy2.size()) {
+//                count++;
+//            }
+//        }
+//
+//        answer = count;
+//
+//        return answer;
+//    }
+
     public static int solution(int[] topping) {
-        int answer = -1;
+        int answer = 0;
+        int[] younger = new int[10001];
+        int[] older = new int[10001];
 
-        HashMap<Integer, Integer> chulSu = new HashMap<>();
-        HashMap<Integer, Integer> dongSaeng = new HashMap<>();
+        int youngCnt = 0, oldCnt = 0;
 
-        int mid = topping.length / 2;
+        // 1. 토핑을 형이 모두 차지한다.
+        for (int t : topping) older[t]++;
 
-        int count = 0;
-        for (int i = 0; i < mid; i++) {
-            chulSu.put(topping[i], chulSu.getOrDefault(topping[i], 0) + 1);
+        // 2. 토핑의 가짓 수를 센다.
+        for (int i : older) oldCnt += i > 0 ? 1 : 0;
+
+        // 3. 다시 주어진 토핑 배열을 보면서 형에게 뺏어 동생에게 주며 토핑의 가짓수가 같아지는 순간에 정답의 가짓수를 추가한다.
+        for (int t : topping) {
+            older[t]--;
+            if (older[t] == 0) oldCnt--;
+            if (younger[t] == 0) youngCnt++;
+            younger[t]++;
+            if (youngCnt == oldCnt) answer++;
         }
-
-        for (int i = mid; i < topping.length; i++) {
-            dongSaeng.put(topping[i], dongSaeng.getOrDefault(topping[i], 0) + 1);
-        }
-
-        if(chulSu.size() == dongSaeng.size()) {
-            count++;
-        }
-
-        HashMap<Integer, Integer> chulSuCopy1 = new HashMap<>(chulSu);
-        HashMap<Integer, Integer> dongSaengCopy1 = new HashMap<>(dongSaeng);
-
-        for (int i = 0; i < mid; i++) {
-            chulSuCopy1.put(topping[mid - i], chulSuCopy1.getOrDefault(topping[mid - i], 0) - 1);
-            dongSaengCopy1.put(topping[mid - i], dongSaengCopy1.getOrDefault(topping[mid - i], 0) + 1);
-
-            if(chulSuCopy1.get(topping[mid - i]) == 0 ) {
-                chulSuCopy1.remove(topping[mid - i]);
-            }
-
-            if(dongSaengCopy1.get(topping[mid - i]) == 0 ) {
-                dongSaengCopy1.remove(topping[mid - i]);
-            }
-
-            if(chulSuCopy1.size() == dongSaengCopy1.size()) {
-                count++;
-            }
-
-        }
-
-        HashMap<Integer, Integer> chulSuCopy2 = new HashMap<>(chulSu);
-        HashMap<Integer, Integer> dongSaengCopy2 = new HashMap<>(dongSaeng);
-
-        for (int i = mid; i < topping.length - 1; i++) {
-            chulSuCopy2.put(topping[i], chulSuCopy2.getOrDefault(topping[i], 0) + 1);
-            dongSaengCopy2.put(topping[i], dongSaengCopy2.getOrDefault(topping[i], 0) - 1);
-
-            if(chulSuCopy2.get(topping[i]) == 0 ) {
-                chulSuCopy2.remove(topping[i]);
-            }
-
-            if(dongSaengCopy2.get(topping[i]) == 0 ) {
-                dongSaengCopy2.remove(topping[i]);
-            }
-
-            if(chulSuCopy2.size() == dongSaengCopy2.size()) {
-                count++;
-            }
-        }
-
-        answer = count;
 
         return answer;
     }
-
 }
--- Java/programmers/level2/week4/PG_level2_석유시추.java
+++ Java/programmers/level2/week4/PG_level2_석유시추.java
@@ -4,6 +4,14 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class PG_level2_석유시추 {
+
+    public static int ans;
+    public static int count;
+    public static boolean[][] visited;
+
+    public static int[] dx = {0, 0, -1, 1};
+    public static int[] dy = {-1, 1, 0, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -34,9 +42,47 @@ public class PG_level2_석유시추 {
     public static int solution(int[][] land) {
         int answer = 0;
 
+        for (int i = 0; i < land[0].length; i++) {
+            visited = new boolean[land.length][land[0].length];
+            count = 0;
 
+            for (int j = 0; j < land.length; j++) {
+                if(land[j][i] == 1) {
+                    dfs(land, j, i);
+                }
+            }
+
+            ans = Math.max(ans, count);
+
+        }
+
+        answer = ans;
 
         return answer;
+    }
+
+    private static void dfs(int[][] land, int y, int x) {
+        if (y == land.length) {
+            ans = Math.max(ans, count);
+            return;
+        }
+
+        visited[y][x] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || nx >= land[0].length || ny < 0 || ny >= land.length) continue;
+
+            if(!visited[ny][nx] && land[ny][nx] == 1) {
+                visited[ny][nx] = true;
+                count++;
+                dfs(land, ny, nx);
+            }
+        }
+
+
     }
 
 }
