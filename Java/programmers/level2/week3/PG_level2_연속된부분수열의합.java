6ba0091b790aaf50df378ca6cc723880edaedb34
atimaby28 <atimaby28@gmail.com>
2024-07-04 19:07:45 +0900

    Update : Algorithm

--- Java/programmers/level2/week3/PG_level2_연속된부분수열의합.java
+++ Java/programmers/level2/week3/PG_level2_연속된부분수열의합.java
@@ -32,23 +32,34 @@ public class PG_level2_연속된부분수열의합 {
 
     }
 
-    public static int[] solution(int[] sequence, int k) {
-        int[] answer = new int[2];
-
-        int[] dp = new int[sequence.length];
-
-        int diff = 1000001;
-        for (int i = 2; i < sequence.length; i++) {
-            dp[i - 1] = sequence[i - 2] + sequence[i - 1];
-            for (int j = i; j < sequence.length; j++) {
-                dp[j] = dp[j - 1] + sequence[j];
-
-            }
-
-        }
-
-        return answer;
-    }
+    // 시간초과 코드
+//    public static int[] solution(int[] sequence, int k) {
+//        int[] answer = {0, 0};
+//
+//        int diff = sequence.length;
+//        for (int i = 0; i < sequence.length; i++) {
+//            int sum = 0;
+//            for (int j = i; j >= 0; j--) {
+//                if(sequence[j] <= sequence[i]) {
+//                    sum += sequence[j];
+//
+//                    if(sum == k && Math.abs(i - j) < diff) {
+//                        diff = Math.abs(i - j);
+//                        answer = new int[]{j, i};
+//
+//                    }
+//                }
+//            }
+//
+//        }
+//        return answer;
+//    }
 
 
 }
+
+/*
+Testcase 33의 반례
+[2, 2, 2, 2, 2], 2
+i와 j를 0부터 시작해야 한다.
+ */
