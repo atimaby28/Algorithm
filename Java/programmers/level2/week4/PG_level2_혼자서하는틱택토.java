c69c71de2697b2b7afb4b7e3c2994d4a9af0df6f
atimaby28 <atimaby28@gmail.com>
2024-08-06 16:31:44 +0900

    Update : Algorithm

--- Java/programmers/level2/week4/PG_level2_혼자서하는틱택토.java
+++ Java/programmers/level2/week4/PG_level2_혼자서하는틱택토.java
@@ -41,27 +41,52 @@ public class PG_level2_혼자서하는틱택토 {
     private static int isPossible(char[][] tiktactoe) {
 
         int xCnt = 0, oCnt = 0;
-        for (int i = 0; i < tiktactoe.length; i++) {
+        for (char[] chars : tiktactoe) {
             for (int j = 0; j < tiktactoe[0].length; j++) {
-                if(tiktactoe[i][j] == 'O') {
+                if (chars[j] == 'O') {
                     oCnt++;
-                } else if(tiktactoe[i][j] == 'X') {
+                } else if (chars[j] == 'X') {
                     xCnt++;
                 }
             }
         }
 
-        if(oCnt != xCnt) {
-            if(oCnt == 1) {
-                return 1;
-            }
-            return 0;
-        } else {
-            if(oCnt == 3 && xCnt == 3) {
-                return 0;
-            }
-            return 1;
+        /*
+        5가지 조건
+        1. 'X'가 'O' 보다 많을 경우
+        2. 'O'가 'X' 보다 2개 이상 많을 경우
+        3. 'O'가 승리 했을 때, 'X'와 'O'의 갯수가 같을 때
+        4. 'X'가 승리 했을 때, 'O'의 갯수가 'X'의 갯수보다 많을 때
+         */
+
+        if(xCnt > oCnt) return 0;
+        else if(oCnt - xCnt >= 2) return 0;
+
+        if(checkWinner(tiktactoe, 'O')) {
+            if(xCnt == oCnt) return 0;
         }
+
+        if(checkWinner(tiktactoe, 'X')) {
+            if(oCnt > xCnt) return 0;
+        }
+
+        return 1;
+    }
+
+    private static boolean checkWinner(char[][] tiktactoe, char ox) {
+
+        for (int i = 0; i < 3; i++) {
+            // row check
+            if(tiktactoe[i][0] == ox && tiktactoe[i][1] == ox && tiktactoe[i][2] == ox) return true;
+            // column check
+            else if(tiktactoe[0][i] == ox && tiktactoe[1][i] == ox && tiktactoe[2][i] == ox) return true;
+        }
+
+        // diagonal check
+        if(tiktactoe[0][0] == ox && tiktactoe[1][1] == ox && tiktactoe[2][2] == ox) return true;
+        else if(tiktactoe[0][2] == ox && tiktactoe[1][1] == ox && tiktactoe[2][0] == ox) return true;
+
+        return false;
     }
 
 }
