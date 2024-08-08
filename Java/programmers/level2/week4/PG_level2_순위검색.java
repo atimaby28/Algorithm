d2e9fbc59b5d00a0d0c04ecef368f3d7458a954f
atimaby28 <atimaby28@gmail.com>
2024-08-08 17:16:18 +0900

    Update : Algorithm

--- Java/programmers/level2/week4/PG_level2_순위검색.java
+++ Java/programmers/level2/week4/PG_level2_순위검색.java
@@ -1,10 +1,7 @@
 package level2.week4;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.util.*;
 
 public class PG_level2_순위검색 {
 
@@ -37,39 +34,61 @@ public class PG_level2_순위검색 {
 
     }
 
-    public static int[] solution(String[] info, String[] query) {
-        int[] answer = {};
+    static HashMap<String, List<Integer>> map;
 
-        LinkedList<ListNode> linkedList = new LinkedList<>();
+    public static int[] solution(String[] info, String[] query) {
+        int[] answer = new int[query.length];
+        map = new HashMap<String, List<Integer>>();
 
         for (int i = 0; i < info.length; i++) {
-            String[] s = info[i].split(" ");
-             
+            String[] p = info[i].split(" ");
+            makeSentence(p, "", 0);
         }
+
+        for (String key : map.keySet())
+            Collections.sort(map.get(key));
 
         for (int i = 0; i < query.length; i++) {
-            String[] s = query[i].split(" and ");
-
-
-
+            query[i] = query[i].replaceAll(" and ", "");
+            String[] q = query[i].split(" ");
+            answer[i] = map.containsKey(q[0]) ? binarySearch(q[0], Integer.parseInt(q[1])) : 0;
         }
-
 
         return answer;
     }
 
-}
+    // 이분 탐색
+    private static int binarySearch(String key, int score) {
+        List<Integer> list = map.get(key);
+        int start = 0, end = list.size() - 1;
 
-class ListNode {
-    String data;
-    ListNode prev;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (list.get(mid) < score)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
 
-    ListNode(ListNode prev, String data) {
-        this.prev = prev;
-        this.data = data;
+        return list.size() - start;
+    }
+
+    // info가 포함될 수 있는 문장
+    private static void makeSentence(String[] p, String str, int cnt) {
+        if (cnt == 4) {
+            if (!map.containsKey(str)) {
+                List<Integer> list = new ArrayList<Integer>();
+                map.put(str, list);
+            }
+            map.get(str).add(Integer.parseInt(p[4]));
+            return;
+        }
+        makeSentence(p, str + "-", cnt + 1);
+        makeSentence(p, str + p[cnt], cnt + 1);
     }
 
 }
+
 /*
 6
 java backend junior pizza 150
@@ -78,6 +97,7 @@ python frontend senior chicken 150
 cpp backend senior pizza 260
 java backend junior chicken 80
 python backend senior chicken 50
+
 6
 java and backend and junior and pizza 100
 python and frontend and senior and chicken 200
---
+++ Java/programmers/level3/PG_level3_정수삼각형.java
@@ -0,0 +1,60 @@
+package level3;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class PG_level3_정수삼각형 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] triangle = new int[n][];
+
+        for (int i = 0; i < triangle.length; i++) {
+            triangle[i] = new int[i + 1];
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < triangle[i].length; j++) {
+                triangle[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int result = solution(triangle);
+
+        bw.write(result + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static int solution(int[][] triangle) {
+        int answer = 0;
+
+        int[][] dp = new int[triangle.length][triangle.length];
+
+        dp[0][0] = triangle[0][0];
+        for (int i = 1; i < triangle.length; i++) {
+            // 맨 왼쪽
+            dp[i][0] = dp[i - 1][0] + triangle[i][0];
+
+            // 중간
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j];
+            }
+
+            // 맨 오른쪽
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+        }
+
+        for (int i = 0; i < triangle.length; i++) {
+            answer = Math.max(answer, dp[triangle.length - 1][i]);
+        }
+
+        return answer;
+    }
+
+}
