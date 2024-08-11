52aa345b89b1c027dde50b29a1416d63aeb55747
atimaby28 <atimaby28@gmail.com>
2024-08-11 21:00:01 +0900

    Update : Algorithm

--- Java/programmers/level2/week4/PG_level2_숫자블록.java
+++ Java/programmers/level2/week4/PG_level2_숫자블록.java
@@ -6,9 +6,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class PG_level2_숫자블록 {
-
-    static ArrayList<Integer> arrayList = new ArrayList<>();
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -27,37 +25,31 @@ public class PG_level2_숫자블록 {
     }
 
     public static int[] solution(long begin, long end) {
-        int[] answer = {};
+        int[] answer = new int[(int) (end - begin + 1)];
 
-        for (long i = begin; i <= end; i++) {
-            if (i == 1) {
-                arrayList.add(0);
-                continue;
+        for (int i = 0; i <= end - begin; i++) {
+            long n = begin + i;
+
+            if (n == 1) {
+                answer[i] = 0;
             } else {
-                arrayList.add((int) block(i));
+                answer[i] = 1;
+
+                for (int j = 2; j <= Math.sqrt(n); j++) {
+                    if (n % j == 0) {
+                        answer[i] = j;
+
+                        if (n / j <= 10000000) {
+                            answer[i] = (int) (n / j);
+                            break;
+                        }
+                    }
+                }
+
             }
         }
-
-        answer = arrayList.stream().mapToInt(Integer::intValue).toArray();
 
         return answer;
-    }
-
-    private static long block(long n) {
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                arrayList.add(i);
-                if (n / i <= 10_000_000) {
-                    return n / i;
-                }
-            }
-        }
-
-        if (!arrayList.isEmpty()) {
-            return arrayList.getLast();
-        }
-
-        return 1;
     }
 
 }
