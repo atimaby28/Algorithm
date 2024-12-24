65fbe7c29baef5854f4f615c9671a38578f4cfa0
atimaby28 <atimaby28@gmail.com>
2024-12-25 00:20:07 +0900

    Update : Algorithm

---
+++ Java/baekjoon/gold/one/BJ_1016_제곱ㄴㄴ수.java
@@ -0,0 +1,52 @@
+package gold.one;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ_1016_제곱ㄴㄴ수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long min = Long.parseLong(st.nextToken());
+        long max = Long.parseLong(st.nextToken());
+
+        long result = solution(min, max);
+
+        bw.write(result + "\n");
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+
+    private static long solution(long min, long max) {
+        int size = (int) (max - min + 1);
+        boolean[] isSquareFree = new boolean[size];
+
+        for (long i = 2; i <= Math.sqrt(max); i++) {
+            long square = i * i;
+
+            // 제곱수의 배수 중 min 이상에서 시작
+            long start = Math.max(square, (min + square - 1) / square * square);
+
+            for (long j = start; j <= max; j += square) {
+                isSquareFree[(int) (j - min)] = true;
+
+            }
+        }
+
+        // 제곱ㄴㄴ수 개수 세기
+        int count = 0;
+        for (boolean free : isSquareFree) {
+            if (!free) count++;
+        }
+
+        return count;
+    }
+
+
+}
--- Java/baekjoon/silver/one/BJ_1747_소수_팰린드롬.java
+++ Java/baekjoon/silver/one/BJ_1747_소수_팰린드롬.java
@@ -1,9 +1,10 @@
 package silver.one;
 
 import java.io.*;
-import java.util.StringTokenizer;
+
 
 public class BJ_1747_소수_팰린드롬 {
+    static final int MAX = 1_000_000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -19,8 +20,43 @@ public class BJ_1747_소수_팰린드롬 {
     }
 
     private static int solution(int n) {
-        int answer = 0;
+        boolean[] prime = eratosthenes();
 
-        return answer;
+        for (int i = n; i < prime.length; i++) {
+            if(!prime[i]) {
+                if(isPalindrome(i)) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private static boolean isPalindrome(int n) {
+        String number = String.valueOf(n);
+        String reversed = new StringBuilder(number).reverse().toString();
+
+        if(number.equals(reversed)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean[] eratosthenes() {
+        boolean[] isPrime = new boolean[MAX * 2];
+
+        isPrime[0] = isPrime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(MAX); i++) {
+            if(isPrime[i]) continue;
+
+            for (int j = i * i; j < isPrime.length; j += i) {
+                isPrime[j] = true;
+            }
+        }
+
+        return isPrime;
     }
 }
